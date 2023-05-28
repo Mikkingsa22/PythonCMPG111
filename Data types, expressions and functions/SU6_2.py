824adcea56e0256a1fc10feb2113c1947daadf36
Miki King Rsa <120072948+Mikkingsa22@users.noreply.github.com>
2023-05-28 17:44:37 +0200

    Create SU6_2.py

---
+++ Data types, expressions and functions/SU6_2.py
@@ -0,0 +1,11 @@
+##Tebatso_Seshayi_40972364
+
+## A program to ask the user to provide a character as input, then display the ASCII value of that character
+## this is to ask the user to provide an ASCII value (int), then display the ASCII character that corresponds to that value
+
+print("**ASCII and Character conversions**") ##displaying the tittle
+
+character = str(input("Enter  charcter:")) ##asking user to enter the character
+print("The ASCII value is:" ,ord(character)) ##displaying and converting character to ASCII value         
+number = int(input("Enter ASCII Value: ")) ##asking user to enter the ASCII Value
+print("The character value is: " , chr(number)) ## displaying and converting ASCII value characte
