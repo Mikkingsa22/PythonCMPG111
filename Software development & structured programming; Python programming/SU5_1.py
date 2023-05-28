27b721ce7930c2484cffd29063fa26726fe61465
Miki King Rsa <120072948+Mikkingsa22@users.noreply.github.com>
2023-05-28 17:41:46 +0200

    Create SU5_1.py

---
+++ Software development & structured programming; Python programming/SU5_1.py
@@ -0,0 +1,16 @@
+##40972364_Seshayi_Tebatso
+
+username=input("Enter Your username:") ##asking user to enter username
+print(username)
+userpassword=input("Enter your password:")##asking user to enter password
+print(userpassword)
+print("Welcome",username,"your password is",userpassword) ##
+character=input("Enter a character:") ##asking user to enter character
+square=character*7  ##Drawing square
+print("Square")
+print(square)
+print(square)
+print(square)
+print(square)
+print(square)
+print(square)
