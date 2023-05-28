dad6806728b07e23c4b950e50c067578c347320d
Miki King Rsa <120072948+Mikkingsa22@users.noreply.github.com>
2023-05-28 17:42:41 +0200

    Create SU_2.py

---
+++ Software development & structured programming; Python programming/SU_2.py
@@ -0,0 +1,21 @@
+##40972364_Seshayi_Tebatso
+
+print("Concatenated values")
+length=str(input("Enter length for the rectangle:"))##asking user to enter length for rectangle
+
+width=str(input("Enter width for the rectangle:")) ##asking user to enter width for rectangle
+
+result=str(length+width)  ##adding lengh & width as strings 
+print("Result: ",result) ##printing results
+
+
+print("Values to calculate area and perimeter") ##printing "Values to calculate area and perimeter"
+
+length=int(input("Enter length for the rectangle:")) ##asking user to enter length for rectangle
+
+width=int(input("Enter width for the rectangle:"))  ##asking user to enter width for rectangle
+
+area=int(length*width) ## adding formula for calculating perimiter
+perimeter=int((length+width)*2) ##adding formula for calculating perimiter
+print( "Area: ",area) ##printing Area
+print("Perimeter: ",perimeter) ##printing perimeter
